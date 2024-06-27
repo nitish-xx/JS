cc83df156d53a67ea5c5c54032d988b36a42a19b
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2024-06-27 05:15:35 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as a newer version
+
+
+//alert(3+3) // we are using nodjs, not browser
+
+console.log(3+3)
+
+console.log("Nitish")
+
+
+let name="Nitish"
+let age= 18
+let isloggedeIn= false
+
+//primitive datatypes
+//number = 2 to the power 53
+//bigint
+//string =>" "
+//boolean => true/false
+
+//null => standlone value
+//undefined =>
+  
+//symbol => to fond uniqueness
+
+
+//object//
+
+console.log(typeof null)  //object
+console.log(typeof undefined) //undefiend
