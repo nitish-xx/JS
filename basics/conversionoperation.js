e874ebc43e55b854231bf597de4f761d8c24e3dc
Nitish kumar <104833175+nitish-xx@users.noreply.github.com>
2024-06-29 05:54:16 +0000

    conversion

---
+++ basics/conversionoperation.js
@@ -0,0 +1,26 @@
+ // let score ="33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+let isLoggedIn = "";
+let booleanIsloggedIn = Boolean(isLoggedIn)
+ // console.log(booleanIsloggedIn);
+
+// 1=> True; 0 => false
+// "" => false
+// "Nitish" => true
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof someNumber)
